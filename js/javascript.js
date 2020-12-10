68a54b345c197cd8268abc12a68bbb095f376641
Lorenzo-Arnau <skyline03@hotmail.it>
2020-12-10 15:19:18 +0100

    Add welcome alert and better js

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,13 @@
-var name = prompt("Il tuo nome?","");
+var name = prompt("Bentornato su Spotify..Qual'è il tuo nome?","");
 var upgrading = document.getElementById("upgrade");
 var createFor = document.getElementById("create-for");
 var sideName = document.getElementById("side-name");
 var evidence = document.getElementById("navbar").getElementsByTagName("li")[3];
-var welcome = "PREMIUM,BENTORNATO " + name;
-upgrading.innerText = welcome;
-
-
-if (upgrading.textContent == "PREMIUM,BENTORNATO " + name) {
+alert("Bentornato " + name + " è bello rivederti!");
+if (name != "") {
+    var welcome = "PREMIUM,BENTORNATO " + name;
+    upgrading.innerText = welcome;
     evidence.innerText = 'PER '+ name;
     createFor.innerText = 'Creato per '+ name;
     sideName.innerText = name;
-}
+};
